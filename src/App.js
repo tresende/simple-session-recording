003b577ebe95d32af08cea906309d00e5e17299c
Thiago Resende <thiago.gcresende@gmail.com>
2019-10-21 22:55:37 -0300

    colocando um debounce

--- src/App.js
+++ src/App.js
@@ -1,8 +1,19 @@
 import React from 'react';
+import { record } from 'rrweb';
 
 import Routes from './routes'
 
 import './styles.css';
+
+const intervals = [];
+record({
+  emit(event) {
+    intervals.map(item => clearInterval(item));
+    intervals.push(setTimeout(() => {
+      console.log(event);
+    }, 500))
+  },
+});
 
 const App = () => (
   <div className="App">
