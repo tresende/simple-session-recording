51688a23bd0d5734b3b9095e62e2c6df3f40bcf8
Thiago Resende <thiago.resende@dtidigital.com.br>
2019-10-22 21:29:13 -0300

    heatmap

--- src/App.js
+++ src/App.js
@@ -1,24 +1,76 @@
-import React from 'react';
+import React, { Component } from 'react'
 import { record } from 'rrweb';
-
-import Routes from './routes'
+import bg from './assets/bg.png';
 
 import './styles.css';
+import Heatmap from './components/Heatmap';
 
-const intervals = [];
-record({
-  emit(event) {
-    intervals.map(item => clearInterval(item));
-    intervals.push(setTimeout(() => {
-      console.log(event);
-    }, 500))
-  },
-});
+export default class Main extends Component {
 
-const App = () => (
-  <div className="App">
-    <Routes />
-  </div>
-);
+  constructor(props) {
+    super(props);
+    this.simpleHeatRef = React.createRef();
+    this.data = []
+    this.state = {
+      data: [],
+      refresh: new Date()
+    }
+  }
 
-export default App;
+  componentDidMount() {
+
+    record({
+      emit: (event) => {
+        console.log(event)
+        if (event.data.positions) {
+          let ocurreces = 0;
+          let x = event.data.positions[0].x;
+          let y = event.data.positions[0].y;
+
+          if (x % 2 == 1)
+            x--;
+
+          if (y % 2 == 1)
+            x--;
+
+          this.data.forEach(item => {
+            if (item[0] === x && item[1] === y) {
+              ocurreces++;
+            }
+          })
+          this.data.push([x, y, ocurreces]);
+        }
+      },
+    });
+
+    setInterval(() => {
+      this.setState({ refresh: new Date() });
+    }, 250);
+  }
+
+  // refresh = () => {
+  //   this.setState({ refresh: new Date() });
+  // }
+
+  render() {
+    return (
+      <div>
+        <img src={bg} />
+        {/* <span>{this.state.refresh.toLocaleTimeString()}</span> */}
+        {/* <button onClick={() => {
+          this.refresh()
+        }}>Refresh</button> */}
+        <Heatmap
+          id={this.data.length}
+          ref={this.simpleHeatRef}
+          width={window.screen.width}
+          height={window.screen.height}
+          data={this.data.concat([])}
+          maxOccurances={20}
+          blur={100}
+          radius={120} />
+
+      </div>
+    )
+  }
+}
